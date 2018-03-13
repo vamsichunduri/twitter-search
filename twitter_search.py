0787e0cdf11186c25f54e0356d5639774ed1ea07
vamsi yaswant chunduri <34867324+vamsichunduri@users.noreply.github.com>
2018-03-13 15:51:21 -0500

    Update twitter_search.py

--- twitter_search.py
+++ twitter_search.py
@@ -8,10 +8,6 @@ import sys
 
 
 '''
-In order to use this script you should register a data-mining application
-with Twitter.  Good instructions for doing so can be found here:
-http://marcobonzanini.com/2015/03/02/mining-twitter-data-with-python-part-1/
-
 After doing this you can copy and paste your unique consumer key,
 consumer secret, access token, and access secret into the load_api()
 function below.
